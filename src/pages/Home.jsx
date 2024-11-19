7df035a78ee9917f14a26951782bb5b018da5273
Stephen Castaneda <stephencastaneda@MacBookPro.lan>
2024-11-19 15:27:12 -0800

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Description from '../components/Description'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Steps />
+        <Description />
+    </div>
+  )
+}
+
+export default Home
